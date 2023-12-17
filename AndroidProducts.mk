de8f87e9fa57337d216cb5cdd810c7c5325a71ef
VistaSlayer <Bakoubak85@gmail.com>
2023-12-17 23:41:50 +0100

    Fix AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,13 +16,18 @@
 
 
 PRODUCT_MAKEFILES += \
-
-        lineage_lucyzh-userdebug:$(LOCAL_DIR)/s9863a1h10_nosec.mk \
-        lineage_lucyzh-user:$(LOCAL_DIR)/s9863a1h10_nosec.mk \
-        lineage_lucyzh-eng:$(LOCAL_DIR)/s9863a1h10_nosec.mk \
+        lineage_lucyzh_Natv:$(LOCAL_DIR)/s9863a1h10_nosec.mk \
+        lineage_lucyzh_Tsg:$(LOCAL_DIR)/s9863a1h10_nosec.mk \
+        lineage_lucyzh_nosec:$(LOCAL_DIR)/s9863a1h10_nosec.mk \
 
 COMMON_LUNCH_CHOICES := \
-        lineage_lucyzh-userdebug \
-        lineage_lucyzh-user \
-        lineage_lucyzh-eng \
+        lineage_lucyzh_Natv-userdebug \
+        lineage_lucyzh_Natv-eng \
+        lineage_lucyzh_Natv-user \
+        lineage_lucyzh_Tsg-userdebug \
+        lineage_lucyzh_Tsg-eng \
+        lineage_lucyzh_Tsg-user \
+        lineage_lucyzh_nosec-userdebug \
+        lineage_lucyzh_nosec-eng \
+        lineage_lucyzh_nosec-user \
 
--- s9863a1h10_Tsg.mk
+++ s9863a1h10_Tsg.mk
@@ -1,5 +1,4 @@
-KERNEL_PATH := kernel/zte/lucyzh
-export KERNEL_PATH
+TARGET_KERNEL_SOURCE := kernel/zte/lucyzh
 
 include device/sprd/sharkl3/s9863a1h10/s9863a1h10_Natv.mk
 
